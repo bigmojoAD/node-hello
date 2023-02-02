8206110bc56281d51e39b8cea26d0d90dd49a6a8
Arton D <ukamed@gmail.com>
2023-02-02 23:40:28 +0100

    changed node.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running at http://0.0.0.0:3000/');
 });
